d6632c2f6663b5cca288711e06e742845aca70d7
Miguel-Parra <miguelts644@hotmail.com>
2018-10-22 09:32:46 -0500

    funciones anonymas

---
+++ 01-Javascript/04-operadores.js
@@ -0,0 +1,19 @@
+
+function ejemplo(){}
+var ejemploDos = function () {}; //Anonymous function
+var miguel = {
+    trabajar: function() {
+        return 'Trabajando';
+    }
+};
+
+var ArregloFunciones =[function(){}];
+
+
+///OPERADORES
+var arregloDeNombres = ['A','b','C'];
+arregloDeNombres.forEach(
+    function (){
+
+    }
+)
