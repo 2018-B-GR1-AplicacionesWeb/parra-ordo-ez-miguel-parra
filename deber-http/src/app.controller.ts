d969cf474356c1787d67a9c3b359946b07c2d2d5
Miguel-Parra <miguelts644@hotmail.com>
2018-12-10 11:28:50 -0500

    cambios deber-http

---
+++ deber-http/src/app.controller.ts
@@ -0,0 +1,38 @@
+import {Get, Controller, Post, Body} from '@nestjs/common';
+import { AppService } from './app.service';
+import {Observable, of} from 'rxjs';
+
+@Controller()
+export class AppController {
+  constructor(private readonly _appService: AppService) {}
+
+  @Get()
+  root(): string {
+    return 'HolaMundo';
+  }
+  @Post('crearMarca')
+  crearMarca(
+      @Body() marca: Marca,
+  ){
+    console.log(marca);
+    // const bdd = this._appService.inicializarBase();
+    const aMarca = this._appService.crearMarca(marca);
+    const respuesta$ = of(JSON.stringify(marca));
+    return (aMarca);
+  }
+}
+
+export interface Auto{
+  numMotor: number;
+  idMarca: number;
+  modelo: string;
+  color: string;
+  fechaLanzamiento: Date;
+}
+
+export interface Marca{
+  idMarca: number;
+  nombre: string;
+  pais: string;
+  fechaFundacion: Date;
+}
---
+++ deber-http/src/app.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@nestjs/common';
+import {Auto, Marca } from './app.controller';
+const fs = require('fs');
+const mergeMap = require ('rxjs/operators').mergeMap; // usando el operador mergemap
+const map = require ('rxjs/operators').map;
+import {Observable, of, from} from 'rxjs';
+
+@Injectable()
+export class AppService {
+  /*root(): string {
+    return 'Hello World!';
+  }*/
+
+  inicializarBase() {
+    const leerBDD$ = from(this.leerBDD()); // 1. creamos un observable para leer la base de datos usando promesa leeRBDD()
+    return leerBDD$
+      .pipe( // Para poder hacer trabajos con este observable
+        mergeMap((RespuestaDeLeerBDD: RespuestaBDD) => { // recibe la respuesta del observable anterior
+          if (RespuestaDeLeerBDD.bdd) {
+            // truty / {}
+            return of(RespuestaDeLeerBDD); // devolviendo un observable
+          } else {
+            // falsy / null
+            return from(this.crearBDD()); // devolviendo un observable
+          }
+        }));
+  }
+
+  leerBDD(){
+    return new Promise(
+      (resolve) => {
+        fs.readFile( 'bdd.json', 'utf-8', (error, contenidoLeido) => {
+          if (error) {
+            resolve({
+              bdd: null,
+            });
+          } else {
+            resolve({
+              bdd: JSON.parse(contenidoLeido),
+            });
+          } }); },
+    );
+  }
+  crearMarca(marca: Marca){
+    this.inicializarBase()
+      .pipe(
+        map(
+          (respuesta: RespuestaBDD) => {
+            respuesta.bdd.MarcasActuales.push(marca)
+            return respuesta;
+          },
+        ),
+        mergeMap(
+          (respuesta: RespuestaBDD) => {
+            return this.guardarBase(respuesta);
+          } ));
+  }
+  guardarBase(bdd: BaseDeDatos) { // bdd de tipo base de datos
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        'bdd.json',  JSON.stringify(bdd),  (err) => {
+          if (err) {
+            reject({
+              mensaje: 'Error guardando BDD',
+              error: 500,
+            });
+          } else {
+            resolve({
+              mensaje: 'Cambio realizados satisfactoriamente',
+            });
+          } } ); } ); }
+  crearBDD() {
+    const contenidoInicialBDD = '{"MarcasActuales":[],"AutosActuales":[]}';
+    return new Promise(
+      (resolve, reject) => {
+        fs.writeFile( 'bdd.json',  contenidoInicialBDD, (err) => {
+          if (err) {
+            reject({
+              mensaje: 'Error creando Base',
+              error: 500 });
+          } else {
+            resolve({
+              bdd: JSON.parse('{"MarcasActuales":[],"AutosActuales":[]}'),
+            });
+          } } ); } ); }
+
+}
+
+
+
+interface BaseDeDatos {
+  MarcasActuales: Marca[];
+  AutosActuales: Auto[];
+}
+
+interface RespuestaBDD {
+  bdd: BaseDeDatos;
+}
